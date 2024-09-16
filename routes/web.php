53992def5f639a83772bafec9c9a3bbdb5ab53e5
Mohamed Gomaa <mhmd2004221@gmail.com>
2024-09-16 16:53:28 +0300

    Add project files

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    if (auth()->check()) {
+        return redirect('/posts');
+    }
+    return view('welcome');
+});
+
+Route::get('/posts', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('posts',PostController::class);
+});
+
+
+
+
+require __DIR__.'/auth.php';
